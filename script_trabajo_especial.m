9fb79d57b14433cfe26839c1a29a2a7685f90094
Ana Ruiz <Ana Ruiz>
2017-06-12 22:59:14 -0300

    segunda actualizacion

--- script_trabajo_especial.m
+++ script_trabajo_especial.m
@@ -1,65 +1,85 @@
 % script_trabajo_especial
+%limpia la command window
 clear
 clc
-tic
 
+
+tic
 dni= 39290709;
 epsilon = 0.1;
-A= [probabilidad, probabilidades_parciales] = probabilidad_fallos(dni, epsilon);
+ [probabilidad, probabilidades_parciales] = probabilidad_fallos(dni, epsilon);
 
 % Mostramos por pantalla la probabilidad que nos dio
 fprintf('Epsilon = %f: \n', epsilon);
-fprintf('Probabilidad de fallos seguidos = %d: \n', probabilidad);
+fprintf('Probabilidad de fallos seguidos = %f \n', probabilidad);
 
 % Graficamos cómo fue evolucionando la probabilidad iteración a iteración
 figure, plot(probabilidades_parciales);
 hold on
-xlabel('Numero de iteracion');
+xlabel('iteracion');
 ylabel('Probabilidad');
 ylim([0 1]);
-legend('Evolucion de la probabilidad');
+legend('probabilidades parciales');
 grid on
+%imprime por pantalla el desvio estandar de las primeras 20 iteraciones
+Y1 = std(probabilidades_parciales(1:20));
+fprintf('desvio estandar de las primeras 20 iteraciones = %f \n', Y1);
+%imprime el desvio estandar de las ultimas 20 iteraciones
+Y2 = std(probabilidades_parciales(end-20:end));
+fprintf('desvio estandar de las ultimas 20 iteraciones = %f \n', Y2 );
+ tiempo_epsilon = toc;
+fprintf('tiempo epsilon 0,1=%f \n', tiempo_epsilon)
 
-fprintf('desvio estandar de las primeras 20 iteraciones = %f: \n', A(1:20));
-fprintf('desvio estandar de las primeras 20 iteraciones = %f: \n', A(end-19:end));
-
+tic
 epsilon = 0.01;
 dni = 39290709;
 
-B =[probabilidad, probabilidades_parciales] = probabilidad_fallos(epsilon, dni);
+[probabilidad, probabilidades_parciales] = probabilidad_fallos(epsilon, dni);
 
-fprintf('Epsilon = %f: \n', epsilon);
-fprintf('Probabilidad de fallos = %f: \n', probabilidad);
+fprintf('Epsilon = %f \n', epsilon);
+fprintf('Probabilidad de fallos = %f \n', probabilidad);
 
 % Grafico
 figure, plot(probabilidades_parciales);
 hold on
-xlabel('Numero de iteracion');
+xlabel('iteracion');
 ylabel('Probabilidad');
 ylim([0 1]);
-legend('Evolucion de la probabilidad');
+legend('probabilidades parciales');
 grid on
 
-fprintf('desvio estandar de las primeras 20 iteraciones = %f: \n', B(1:20));
-fprintf('desvio estandar de las primeras 20 iteraciones = %f: \n', B(end-19:end));
+Y1 = std(probabilidades_parciales(1:20));
+fprintf('desvio estandar de las primeras 20 iteraciones = %f \n', Y1);
 
+Y2 = std(probabilidades_parciales(end-20:end));
+fprintf('desvio estandar de las ultimas 20 iteraciones = %f \n', Y2 );
+tiempo_epsilon = toc;
+fprintf('tiempo epsilon 0,01=%f \n', tiempo_epsilon)
+
+tic
 epsilon = 0.001;
 dni = 39290709;
 
- C = [probabilidad, probabilidades_parciales] = probabilidad_fallos(epsilon, dni);
+[probabilidad, probabilidades_parciales] = probabilidad_fallos(epsilon, dni);
 
-fprintf('Epsilon = %f: \n', epsilon);
-fprintf('Probabilidad de fallos= %f: \n', probabilidad);
+fprintf('Epsilon = %f \n', epsilon);
+fprintf('Probabilidad de fallos= %f \n', probabilidad);
 
 % Grafico
 figure, plot(probabilidades_parciales);
 hold on
-xlabel('Numero de iteracion');
+xlabel('iteracion');
 ylabel('Probabilidad');
 ylim([0 1]);
-legend('Evolucion de la probabilidad');
+legend('probabilidades parciales');
 grid on
 
-fprintf('desvio estandar de las primeras 20 iteraciones = %f: \n', C(1:20));
-fprintf('desvio estandar de las primeras 20 iteraciones = %f: \n', C(end-19:end));
-toc
+Y1 = std(probabilidades_parciales(1:20));
+fprintf('desvio estandar de las primeras 20 iteraciones = %f \n', Y1);
+
+Y2 = std(probabilidades_parciales(end-20:end));
+fprintf('desvio estandar de las ultimas 20 iteraciones = %f \n', Y2 );
+tiempo_epsilon = toc;
+fprintf('tiempo epsilon 0,001=%f \n', tiempo_epsilon)
+
+ 
